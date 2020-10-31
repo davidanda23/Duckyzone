83507d4ff3e7b847a984f7e2fbc01efc70396280
davidanda23 <david.adwork@outlook.com>
2020-10-31 17:20:16 -0600

    Imagen nueva en home

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -3,13 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 
-import { MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatTabsModule} from '@angular/material/tabs';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
     HomeRoutingModule,
     CommonModule
