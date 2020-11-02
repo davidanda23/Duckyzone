717cbe6fb7817b15320a5bc6bd38ba8ab1ad98f5
davidanda23 <david.adwork@outlook.com>
2020-11-01 20:16:23 -0600

    Carusel en Home

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,6 +2,9 @@ import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
+import { MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTabsModule} from '@angular/material/tabs';
 
 
 
@@ -9,7 +12,10 @@ import { HomeRoutingModule } from './home-routing.module';
   declarations: [HomeComponent],
   imports: [
     HomeRoutingModule,
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatTabsModule
   ]
 })
 export class HomeModule { }
