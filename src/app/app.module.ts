f1446dc8bf35d4cbbff43f337fbaf8d419f74fd6
Daniel R <drosalesfl@gmail.com>
2020-11-22 15:09:34 -0600

    Hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { DeportesComponent } from './departamentos/deportes/deportes.component';
+import { ModaComponent } from './departamentos/moda/moda.component';
+import { ElectronicosComponent } from './departamentos/electronicos/electronicos.component';
+import { MusicaComponent } from './departamentos/musica/musica.component';
+import { BellezaComponent } from './departamentos/belleza/belleza.component';
+import { HogarComponent } from './departamentos/hogar/hogar.component';
+import { NinosComponent } from './departamentos/ninos/ninos.component';
+import { ComidaComponent } from './departamentos/comida/comida.component';
+import { LibrosComponent } from './departamentos/libros/libros.component'
 
 //import { HttpService } from './services/HttpService/http-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent,NavbarComponent
+    AppComponent,NavbarComponent, DeportesComponent, ModaComponent, ElectronicosComponent, MusicaComponent, BellezaComponent, HogarComponent, NinosComponent, ComidaComponent, LibrosComponent
   ],
   imports: [
     BrowserModule,
